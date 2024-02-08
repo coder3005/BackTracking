4510141336f5f654f2ddb6c70fc3d8938561b229
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-08 18:21:38 +0530

    Create gridways.java

---
+++ gridways.java
@@ -0,0 +1,25 @@
+//numbers of ways to reach from (0,0) to (n-1,m-1) in a n*m grid. allowed moves- right or down.
+package BackTracking;
+
+public class solution {
+    public static int gridWays(int i, int j, int n, int m) {
+        //base case
+        if(i==n-1 && j==m-1) { //condition for last cell
+            return 1;
+        }
+        else if(i==n || j==n) { //boundary cross condition
+            return 0;
+        }
+        int w1=gridWays(i+1,j,n,m);
+        int w2=gridWays(i,j+1,n,m);
+        return w1+w2;
+    }
+    public static void main(String[] args) {
+        int n=3,m=3;
+        System.out.println(gridWays(0,0,n,m));
+    }
+}
+
+// TC -> O(2^(n+m)) => unoptimized code
+
+
