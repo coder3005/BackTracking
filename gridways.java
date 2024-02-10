fa0579547b855af8f105328a8725e70be0f9fafc
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-10 12:49:23 +0530

    Update gridways.java

--- gridways.java
+++ gridways.java
@@ -10,8 +10,8 @@ public class solution {
         else if(i==n || j==n) { //boundary cross condition
             return 0;
         }
-        int w1=gridWays(i+1,j,n,m);
-        int w2=gridWays(i,j+1,n,m);
+        int w1=gridWays(i+1,j,n,m); //way1
+        int w2=gridWays(i,j+1,n,m); //way2
         return w1+w2;
     }
     public static void main(String[] args) {
